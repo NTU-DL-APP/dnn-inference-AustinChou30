a714bb33acfebbd67182a427f8bb91977d7f090e
AustinChou1014 <409410635@gms.tku.edu.tw>
2025-06-10 14:51:47 +0800

    113C52011_v3

--- test.py
+++ test.py
@@ -8,16 +8,18 @@ os.makedirs("model", exist_ok=True)
 
 # === 載入資料集 ===
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
-x_train = x_train / 255.0
-x_test = x_test / 255.0
+x_train = (x_train / 255.0 - 0.5) * 2
+x_test = (x_test / 255.0 - 0.5) * 2
 
 # === 建立模型 ===
 model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28), name="flatten"),
-    tf.keras.layers.Dense(256, activation='relu', name="dense_1"),
-    tf.keras.layers.Dense(128, activation='relu', name="dense_2"),
-    tf.keras.layers.Dense(64, activation='relu', name="dense_3"),
-    tf.keras.layers.Dense(10, activation='softmax', name="output")
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
 ])
 
 # === 編譯與訓練 ===
@@ -25,7 +27,7 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=30, batch_size=64, validation_split=0.01)
+model.fit(x_train, y_train, epochs=30, batch_size=64, validation_split=0.1)
 
 # === 測試模型準確率 ===
 test_loss, test_acc = model.evaluate(x_test, y_test)
